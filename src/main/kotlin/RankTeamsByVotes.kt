84039eafcb56754bd291e4245540e9226a08d5a5
bondard <dmytro.bondarenko@bsci.com>
2022-10-17 22:38:38 -0700

    Added solution for RankTeamsByVotes

---
+++ src/main/kotlin/RankTeamsByVotes.kt
@@ -0,0 +1,36 @@
+fun main() {
+    val votes = arrayOf("ABC","ACB","ABC","ACB","ACB")
+    rankTeams(votes)
+}
+
+fun rankTeams(votes: Array<String>): String {
+    val maxTeams = 26
+    val numberOfTeams = votes[0].length
+    var ranks = arrayOf<IntArray>()
+    for (i in 0..maxTeams) {
+        val array = IntArray(numberOfTeams) { 0 }
+        ranks += array
+    }
+
+    votes.forEach { vote ->
+        vote.forEachIndexed { place, team ->
+            val teamNumber = team - 'A'
+            ranks[teamNumber][place]++
+        }
+    }
+
+    val teams = votes[0].toCharArray()
+    val sorted = teams.sortedWith { t1, t2 ->
+        for (i in 0 until numberOfTeams) {
+            val t1Number = t1 - 'A'
+            val t2Number = t2 - 'A'
+            if (ranks[t1Number][i] != ranks[t2Number][i]) {
+                return@sortedWith ranks[t2Number][i] - ranks[t1Number][i]
+            }
+        }
+        t1 - t2
+    }
+
+    return String(sorted.toCharArray())
+}
+
---
+++ src/test/kotlin/RankTeamsByVotesTest.kt
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class RankTeamsByVotesTest {
+    @Test
+    fun testOne() {
+        val votes = arrayOf("ABC","ACB","ABC","ACB","ACB")
+        val expected = "ACB"
+        assertEquals(expected, rankTeams(votes))
+    }
+
+    @Test
+    fun testTwo() {
+        val votes = arrayOf("WXYZ","XYZW")
+        val expected = "XWYZ"
+        assertEquals(expected, rankTeams(votes))
+    }
+
+    @Test
+    fun testThree() {
+        val votes = arrayOf("BCA","CAB","CBA","ABC","ACB","BAC")
+        val expected = "ABC"
+        assertEquals(expected, rankTeams(votes))
+    }
+}
