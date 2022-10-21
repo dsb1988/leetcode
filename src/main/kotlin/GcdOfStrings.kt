30501554a9455a91d34aba773929e48059308a90
bondard <dmytro.bondarenko@bsci.com>
2022-10-20 20:20:28 -0700

    Added GcdOfStrings.kt

---
+++ src/main/kotlin/GcdOfStrings.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val str1 = "ABCABC"
+    val str2 = "ABC"
+    gcdOfStrings(str1, str2)
+}
+
+fun gcdOfStrings(str1: String, str2: String): String {
+    return if (str1+str2 != str2+str1) {
+        ""
+    } else {
+        str1.substring(0, gcd(str1.length, str2.length))
+    }
+}
+
+fun gcd(a: Int, b: Int): Int = if (a == 0) b else gcd(b % a, a)
