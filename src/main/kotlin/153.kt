9db64e31d35ab53b2947b6ba7551597bc750ead2
bondard <dmytro.bondarenko@bsci.com>
2022-10-26 20:03:31 -0700

    Added 153 solution

---
+++ src/main/kotlin/153.kt
@@ -0,0 +1,15 @@
+fun findMin(nums: IntArray): Int {
+    if (nums.size == 1) return nums[0]
+    if (nums.isEmpty()) return 0
+    var left = 0
+    var right = nums.size - 1
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (nums[mid] < nums[right]) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return nums[left]
+}
