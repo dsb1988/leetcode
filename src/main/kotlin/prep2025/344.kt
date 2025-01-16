ce2ac78254911c480ece9191703737ab2bc313a2
Dmytro Bondarenko <d.s.bondarenko@gmail.com>
2025-01-16 08:59:56 -0800

    344

---
+++ src/main/kotlin/prep2025/344.kt
@@ -0,0 +1,13 @@
+package prep2025
+
+fun reverseString(s: CharArray): Unit {
+    var left = 0
+    var right = s.lastIndex
+    while (left < right) {
+        val temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        left++
+        right--
+    }
+}
