5adef98c9b04410a086e45f55fcae53355ed2eab
Dmytro Bondarenko <d.s.bondarenko@gmail.com>
2025-01-16 15:24:58 -0800

    Update 1010

--- src/main/kotlin/prep2025/1010.kt
+++ src/main/kotlin/prep2025/1010.kt
@@ -4,17 +4,17 @@ fun main() {
     println(numPairsDivisibleBy60(intArrayOf(30, 20, 150, 100, 40)))
 }
 
-fun numPairsDivisibleBy60(time: IntArray): Int {
-    val rems = Array(60) { 0 }
+fun numPairsDivisibleBy60(time: IntArray): Long {
+    val rems = Array(60) { 0L }
     time.forEach { t ->
         val rem = t.mod(60)
-        rems[rem] += 1
+        rems[rem] += 1L
     }
 
-    var result = 0
-    result += ((rems[0] - 1) * rems[0]) / 2
+    var result: Long = 0
+    result += ((rems[0] - 1) * rems[0]) / 2 // ((n-1) * n) / 2
     result += ((rems[30] - 1) * rems[30]) / 2
-    for (i in 1 until 29) {
+    for (i in 1 until 30) {
         result += rems[i] * rems[60 - i]
     }
     return result
